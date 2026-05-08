99fba4e41d1bc12c7de2ebce9507eb76d2ed131b
Lauty <lautarochacon99@gmail.com>
2026-05-08 18:08:28 -0300

    agregue prompt

---
+++ Presentacion.js
@@ -0,0 +1,3 @@
+const prompt = require("prompt-sync")();
+console.log ("Mi nombre es: Lautaro");
+console.log ("Este es mi segundo repositorio de git hub");
--- Pruebas/Presentacion.js
+++
@@ -1,3 +0,0 @@
-console.log ("Mi nombre es: Lautaro");
-console.log ("Este es mi segundo repositorio de git hub");
-console.log ("Sigo experimentando con git hub y aprendiendo a usarlo");
