42b126b4a991e8ffbc566db80ced3a15c6c5d483
Lauty <lautarochacon99@gmail.com>
2026-05-10 16:02:43 -0300

    se agrego la edad a prueba prompt

--- Pruebaprompt.js
+++ Pruebaprompt.js
@@ -1,5 +1,7 @@
 const prompt = require("prompt-sync")();
 
 let nombre = prompt("Ingrese su nombre: ");
+let edad = prompt("Ingrese su edad: ");
 
 console.log("Hola " + nombre);
+console.log("Tienes " + edad + " años");
